2f7d85bf91b6242f9167f192e4f741fed8ee136e
LohJian <zogmin925@gmail.com>
2025-09-19 20:05:44 +0800

    Added base program with mood input

---
+++ music_recommender.py
@@ -0,0 +1,13 @@
+import time 
+
+# 🎶 welcome message first
+
+print("🎶 Welcome to Music Genre Recommender 🎶")
+time.sleep(0.5)
+print("We will suggest a music genre based on your mood!👨🏻‍🎤")
+time.sleep(2)
+
+mood=input("How was your feeling today?(sad, happy, relaxed or angry,etc.):")
+print(mood)
+time.sleep(0.8)
+print("Got it! You are feeling",mood)
