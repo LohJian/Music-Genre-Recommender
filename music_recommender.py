cce2e3f221934b49a075b0fb096bd47e9f684f3b
Terrence Chin Jia Heng <terrence.chin28@gmail.com>
2025-09-19 21:31:02 +0800

    add Recommendation Logic

--- music_recommender.py
+++ music_recommender.py
@@ -1,4 +1,5 @@
 import time 
+import random
 
 # 🎶 welcome message first
 
@@ -7,7 +8,19 @@ time.sleep(0.5)
 print("We will suggest a music genre based on your mood!👨🏻‍🎤")
 time.sleep(2)
 
-mood=input("How was your feeling today?(sad, happy, relaxed or angry,etc.):")
+mood=input("How was your feeling today?(sad, happy, relaxed or energetic,etc.):")
 print(mood)
 time.sleep(0.8)
 print("Got it! You are feeling",mood)
+
+
+
+music_db = {
+    "happy": ["Pop - Taylor Swift", "Dance - BTS", "K-Pop - BLACKPINK"],
+    "sad": ["Blues - B.B. King", "Lo-Fi - Chillhop", "Jazz - Miles Davis"],
+    "energetic": ["Rock - Linkin Park", "EDM - Martin Garrix", "Hip-Hop - Eminem"],
+    "relaxed": ["Classical - Mozart", "Chillhop - Lofi Beats", "Acoustic - Ed Sheeran"]
+}
+
+suggestion = random.choice(music_db[mood])
+print(f"\n✅ Based on your mood({mood}), we recommend: {suggestion} 🎧")
